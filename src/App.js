79e8301e3d8d630d223ffd28fc7aa269c21b23d1
FerhiSeif <seif.ferhi@ouicoding.com>
2020-06-24 17:53:05 +0200

    pricing and distributor page integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
-//import Chart from './components/chart'
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home'
+import Pricing from './pages/Pricing'
+import Distributor from './pages/Distributor'
 import "./App.css";
 import { useTranslation } from "react-i18next";
 import i18next from "i18next";
@@ -8,14 +10,16 @@ import i18next from "i18next";
 function App() {
   const { t } = useTranslation();
 
-  function handleClick(lang) {
-    i18next.changeLanguage(lang);
-  }
-
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/Pricing" component={Pricing} />
+    <Route exact path="/distributor" component={Distributor} />
+    {/* <Route exact path="/nos-partenaires" component={Partenaires} />
+     <Route exact path="/notre-blog" component={Blog} />
+     <Route exact path="/contact" component={Rejoindre} />
+     <Route exact path="/CGU" component={CGU} /> */}
+  </Switch>
   );
 }
 
